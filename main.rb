4b15d062fddd7e09c7a08effb437229502591893
康平大塚 <dashengkangping@gmail.com>
2019-06-21 17:21:14 +0900

    relationsも取込できる

--- main.rb
+++ main.rb
@@ -16,13 +16,27 @@ opt.parse(ARGV)
 
 data = Rubrowser::Data.new(params[:path])
 
-# pp data.definitions[0..20]
-# pp data.relations
+# ex: {"FirstClass": 1, "SecondClass": 2}
+node_hash = {}
+data.definitions.each_with_index do |definition, index|
+  node_hash[definition.to_s] = index + 1
+end
 
 # write nodes.csv
 CSV.open('neo4j/import/nodes_ruby.csv', 'wb') do |csv|
   csv << %w(id name path)
   data.definitions.each_with_index do |definition, index|
     csv << [ index + 1, definition.to_s, definition.file]
+  end
+end
+
+# write relations.csv
+CSV.open('neo4j/import/relations_ruby.csv', 'wb') do |csv|
+  csv << %w(from to)
+  data.relations.each do |relation|
+    from = node_hash[relation.caller_namespace.to_s]
+    to = node_hash[relation.namespace.to_s]
+    next if from.nil? || to.nil?
+    csv << [from, to]
   end
 end
