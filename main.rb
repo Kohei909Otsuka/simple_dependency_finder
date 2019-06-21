e989db331c57a456b2c8572dfea010db803a5cea
Kohei909Otsuka <kohei.otsuka@integrity-healthcare.co.jp>
2019-06-21 14:50:50 +0900

    とりあえずnodesだけ取込できる

--- main.rb
+++ main.rb
@@ -1,4 +1,7 @@
+require 'bundler/setup'
 require 'optparse'
+require 'rubrowser' # TODO: あとで必要なところだけcopyしてくる
+require 'csv'
 
 opt = OptionParser.new
 
@@ -9,6 +12,17 @@ params = {
 opt.on('-path', '--path', Array, 'abs path to search comma separated') do |v|
   params[:path] = v
 end
-
 opt.parse(ARGV)
-p params
+
+data = Rubrowser::Data.new(params[:path])
+
+# pp data.definitions[0..20]
+# pp data.relations
+
+# write nodes.csv
+CSV.open('neo4j/import/nodes_ruby.csv', 'wb') do |csv|
+  csv << %w(id name path)
+  data.definitions.each_with_index do |definition, index|
+    csv << [ index + 1, definition.to_s, definition.file]
+  end
+end
